9ae9a5bb8948d95caa3452f363d50aea6bd2536e
sofinista <105441648+sofinista@users.noreply.github.com>
2026-09-22 15:58:00 -0500

    Proyecto base generado con Spring Initializr

---
+++ src/test/java/com/floristeria/bloom/BloomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.floristeria.bloom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
